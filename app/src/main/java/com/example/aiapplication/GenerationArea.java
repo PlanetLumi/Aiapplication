be2db70b88547cc808efa19afb13c7d9a7593e94
Planet Lumi <jimmyminns01@gmail.com>
2024-11-16 21:26:12 +0000

    Added new text views nothing special;

--- app/src/main/java/com/example/aiapplication/GenerationArea.java
+++ app/src/main/java/com/example/aiapplication/GenerationArea.java
@@ -2,10 +2,21 @@ package com.example.aiapplication;
 
 import android.os.Bundle;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class GenerationArea extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.generation_area);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.generationArea), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }

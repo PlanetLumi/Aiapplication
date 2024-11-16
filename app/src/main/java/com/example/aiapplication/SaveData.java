14a979febead9923de60305077eed9c9a925249b
Planet Lumi <jimmyminns01@gmail.com>
2024-11-16 00:43:27 +0000

    First push with no ignores please

--- app/src/main/java/com/example/aiapplication/SaveData.java
+++ app/src/main/java/com/example/aiapplication/SaveData.java
@@ -3,6 +3,7 @@ package com.example.aiapplication;
 import static android.content.Context.MODE_PRIVATE;
 
 import android.content.Context;
+import android.content.Intent;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -10,6 +11,11 @@ import java.io.OutputStreamWriter;
 
 public class SaveData{
     public void FileSave(Context context, String userData) throws IOException {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TITLE, "UserData.txt");
+        context.startActivity(intent);
         FileOutputStream fileOutput = context.openFileOutput("UserData.txt", MODE_PRIVATE);
         OutputStreamWriter outputWriter = new OutputStreamWriter(fileOutput);
         outputWriter.write(userData);

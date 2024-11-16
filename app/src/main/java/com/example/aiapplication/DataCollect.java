5bdf4ced335c82c9316919dc294d0bb14bf06ce0
Planet Lumi <jimmyminns01@gmail.com>
2024-11-16 02:24:54 +0000

    Working Data Collection text file test, added Request Page outline

--- app/src/main/java/com/example/aiapplication/DataCollect.java
+++ app/src/main/java/com/example/aiapplication/DataCollect.java
@@ -1,5 +1,6 @@
 
 package com.example.aiapplication;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -46,15 +47,24 @@ public class DataCollect extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                SaveData saveData = new SaveData();
-                try {
-                    saveData.FileSave(DataCollect.this, gatherData());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                ReadData readData = new ReadData();
-                TextView testDisplay = findViewById(R.id.test);
-                testDisplay.setText(readData.returnData(DataCollect.this, "UserData.txt"));
+                 SaveData saveData = new SaveData();
+            try {
+               saveData.FileSave(DataCollect.this, gatherData());
+            } catch (IOException e) {
+            throw new RuntimeException(e);
+            }
+            ReadData readData = new ReadData();
+            TextView testDisplay = findViewById(R.id.test);
+            testDisplay.setText(readData.returnData(DataCollect.this, "UserData.txt"));
+          }
+        });
+
+        Button exit = findViewById(R.id.Exit);
+        exit.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick (View v){
+                    startActivity(new Intent(DataCollect.this,MainActivity.class));
             }
         });
     }

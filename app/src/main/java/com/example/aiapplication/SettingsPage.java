caec8a5d45331c76e70caa96b715e34401ed2502
Planet Lumi <jimmyminns01@gmail.com>
2024-11-17 21:22:37 +0000

    Added Settings to manifest, also made Spinner and Switch functions, spinner is fully integrated, switch currenty does not have use

--- app/src/main/java/com/example/aiapplication/SettingsPage.java
+++ app/src/main/java/com/example/aiapplication/SettingsPage.java
@@ -9,14 +9,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SettingsPage extends AppCompatActivity {
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    EdgeToEdge.enable(this);
-    setContentView(R.layout.activity_main);
-    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-        Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-        v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-        return insets;
-    });
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.settingspage);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        spinnerFunc.setSpinner(SettingsPage.this, R.id.stylePalette, R.array.style_palette);
+        saveButtonFunc.saveBtn(SettingsPage.this, SettingsPage.this, new String[]{"stylePalette", "permissions", "notifications"}, "settings.txt");
+        ExitButtonFunc.exitBtn(SettingsPage.this, MainActivity.class);
     }
 }

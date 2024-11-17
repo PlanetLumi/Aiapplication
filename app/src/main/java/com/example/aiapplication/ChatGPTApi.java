bc5132101556562d38c7d921f97bbd92680f578b
Planet Lumi <jimmyminns01@gmail.com>
2024-11-17 01:13:35 +0000

    integrating base ChatGPT Api systems and class functions calls

---
+++ app/src/main/java/com/example/aiapplication/ChatGPTApi.java
@@ -0,0 +1,13 @@
+package com.example.aiapplication;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import okhttp3.ResponseBody;
+
+public interface ChatGPTApi {
+    @Headers("Content-Type: application/json")
+    @POST("v1/chat/completions")
+    Call<ResponseBody> getChatResponse(@Body RequestBody body);
+}
